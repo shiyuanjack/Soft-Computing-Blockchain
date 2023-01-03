740a3b4632d76d67c16c9fb70223f3b4e1dfb87e
shiyuanjack <79642700+shiyuanjack@users.noreply.github.com>
2023-01-03 21:24:32 +0800

    Delete 代码 directory

--- 代码/后端/backend/application/service/upload_service.py
+++
@@ -1,9 +0,0 @@
-from ..data import user_data
-
-def get_user_key_info(user_name):
-    user_info = user_data.get_user_by_name(user_name)
-    key_info = dict()
-    key_info['des'] = user_info.USER_DES
-    key_info['pk'] = user_info.USER_RSAPK
-    key_info['sk'] = user_info.USER_RSASK
-    return key_info
